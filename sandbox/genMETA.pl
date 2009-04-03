a38e14b7e97af5e1dc505f7ef73e777e67cdb267
H.M. Brand <merijn@a5.(none)>
2009-04-03 22:38:06 +0200

    add configure_requires section to META.yml

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -56,42 +56,48 @@ else {
 
 __END__
 --- #YAML:1.1
-name:              DBD-Unify
-version:           VERSION
-abstract:          DBI driver for Unify database systems
-license:           perl
+name:                    DBD-Unify
+version:                 VERSION
+abstract:                DBI driver for Unify database systems
+license:                 perl
 author:              
     - H.Merijn Brand <h.m.brand@xs4all.nl>
-generated_by:      Author
-distribution_type: module
+generated_by:            Author
+distribution_type:       module
 provides:
     DBD::Unify:
-        file:      lib/DBD/Unify.pm
-        version:   VERSION
+        file:            lib/DBD/Unify.pm
+        version:         VERSION
     DBD::Unify::GetInfo:
-        file:      lib/DBD/Unify/GetInfo.pm
-        version:   0.10
+        file:            lib/DBD/Unify/GetInfo.pm
+        version:         0.10
     DBD::Unify::TypeInfo:
-        file:      lib/DBD/Unify/TypeInfo.pm
-        version:   0.10
+        file:            lib/DBD/Unify/TypeInfo.pm
+        version:         0.10
 requires:     
-    perl:          5.006
-    Carp:          0
-    Cwd:           0
-    DBI:           1.42
-    DynaLoader:    0
+    perl:                5.006
+    Carp:                0
+    Cwd:                 0
+    DBI:                 1.42
+    DynaLoader:          0
+configure_requires:
+    ExtUtils::MakeMaker: 0
+    Config:              0
+    Cwd:                 0
+    DBI:                 1.42
+    DBI::DBD:            0
 build_requires:
-    Config:        0
-    File::Copy:    0
-    File::Find:    0
-    Test::Harness: 0
-    Test::More:    0
+    Config:              0
+    File::Copy:          0
+    File::Find:          0
+    Test::Harness:       0
+    Test::More:          0
 recommends:
-    perl:          5.8.8
-    DBI:           1.607
+    perl:                5.8.8
+    DBI:                 1.607
 resources:
-    license:       http://dev.perl.org/licenses/
-    repository:    http://repo.or.cz/w/DBD-Unify.git
+    license:             http://dev.perl.org/licenses/
+    repository:          http://repo.or.cz/w/DBD-Unify.git
 meta-spec:
-    version:       1.4
-    url:           http://module-build.sourceforge.net/META-spec-v1.4.html
+    version:             1.4
+    url:                 http://module-build.sourceforge.net/META-spec-v1.4.html
