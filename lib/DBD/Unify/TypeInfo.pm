f6672bec1261a3912a6f5dba590194c6afc1a596
H.M. Brand <merijn@a5.(none)>
2009-06-08 19:00:55 +0200

    The generated TypeInfo was very wrong. Corrected.

--- lib/DBD/Unify/TypeInfo.pm
+++ lib/DBD/Unify/TypeInfo.pm
@@ -38,22 +38,68 @@ $type_info_all = [
 	NUM_PREC_RADIX     => 17,
 	INTERVAL_PRECISION => 18,
 	},
-    [ "BINARY",          SQL_LONGVARBINARY,undef,"'",  "'",  undef,            1,0,0,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "BYTE",            SQL_VARBINARY,    undef,"'",  "'",  "MAX LENGTH",     1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "TEXT",            SQL_LONGVARCHAR,  undef,"'",  "'",  undef,            1,0,0,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "CHAR",            SQL_VARCHAR,      undef,"'",  "'",  "MAX LENGTH",     1,1,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "INT",             SQL_INTEGER,      undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    0,    undef,undef,undef,undef, ],
-    [ "SMALLINT",        SQL_SMALLINT,     undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    0,    undef,undef,undef,undef, ],
-    [ "DOUBLE PRECISION",SQL_DOUBLE,       undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
-    [ "FLOAT",           SQL_FLOAT,        undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
-    [ "REAL",            SQL_REAL,         undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
-    [ "TIME",            SQL_TIME,         undef,undef,undef,undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "HUGE DATE",       SQL_DATE,         undef,undef,undef,undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "DATE",            SQL_DATE,         undef,undef,undef,undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
-    [ "HUGE AMOUNT",     SQL_NUMERIC,      undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,2,    2,    undef,undef,undef,undef, ],
-    [ "AMOUNT",          SQL_NUMERIC,      undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,2,    2,    undef,undef,undef,undef, ],
-    [ "CURRENCY",        SQL_DECIMAL,      undef,undef,undef,"PRECISION,SCALE",1,0,3,0,    undef,undef,undef,0,    8,    undef,undef,undef,undef, ],
-    [ "HUGE INT",        SQL_DECIMAL,      undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    0,    undef,undef,undef,undef, ],
+
+#     TYPE_NAME           DATA_TYPE        SIZE  PFX   SFX   PARAMS            N C S UNSIG FPS   AUTO  LOCAL MINSC MAXSC SDT   SDS   RADIX PREC
+#   [ "GIANT AMOUNT",                      undef,undef,undef,"PRECISION,SCALE",1,0,3,0,    undef,undef,undef,2,    2,    undef,undef,undef,undef, ],
+    [ "HUGE AMOUNT",      SQL_REAL,        undef,undef,undef,"PRECISION,SCALE",1,0,3,0,    undef,undef,undef,2,    2,    undef,undef,undef,undef, ],
+    [ "AMOUNT",           SQL_FLOAT,       undef,undef,undef,"PRECISION,SCALE",1,0,3,0,    undef,undef,undef,2,    2,    undef,undef,undef,undef, ],
+    [ "BINARY",           SQL_VARBINARY,   undef,"'", ,"'",  undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
+    [ "BYTE",             SQL_BINARY,      undef,undef,undef,undef,            1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+    [ "CHAR",             SQL_CHAR,        undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+#   [ "CURRENCY",                          undef,undef,undef,"PRECISION,SCALE",1,0,3,0,    2,    undef,undef,0,    8,    undef,undef,undef,undef, ],
+    [ "HUGE DATE",        SQL_TIMESTAMP,   undef,undef,undef,undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
+    [ "DATE",             SQL_DATE,        undef,undef,undef,undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
+    [ "DECIMAL",          SQL_DECIMAL,     undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+    [ "DOUBLE PRECISION", SQL_DOUBLE,      undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+    [ "FLOAT",            SQL_FLOAT,       undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+    [ "HUGE INTEGER",     SQL_BIGINT,      undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    0,    undef,undef,undef,undef, ],
+    [ "INTEGER",          SQL_INTEGER,     undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+    [ "NUMERIC",          SQL_NUMERIC,     undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    0,    undef,undef,undef,undef, ],
+    [ "REAL",             SQL_REAL,        undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    undef,undef,undef,undef,undef, ],
+    [ "SMALLINT",         SQL_SMALLINT,    undef,undef,undef,"PRECISION",      1,0,3,0,    undef,undef,undef,0,    0,    undef,undef,undef,undef, ],
+    [ "TEXT",             SQL_LONGVARCHAR, undef,"'", ,"'",  undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
+    [ "TIME",             SQL_TIME,        undef,undef,undef,undef,            1,0,3,undef,undef,undef,undef,undef,undef,undef,undef,undef,undef, ],
     ];
 
 1;
+
+__END__
+Interactive SQL/A supports the following column data types:
+
+    - [HUGE] AMOUNT [(integer[,2])]
+    - BINARY
+    - BYTE [integer]
+    - CHAR[ACTER] [(integer)]
+    - CURRENCY [(integer[,integer])]
+    - [HUGE] DATE
+    - DEC[IMAL] [(integer)]
+    - DOUBLE PRECISION
+    - FLOAT
+    - HUGE INTEGER
+    - INT[EGER]
+    - NUMERIC [(integer)]
+    - REAL
+    - SMALLINT
+    - TEXT
+    - TIME
+
+#   define SQLNOTYPE	((SQLCOLTYPE)0)
+#   define SQLCHAR	((SQLCOLTYPE)1)
+#   define SQLNUMERIC	((SQLCOLTYPE)2)
+#   define SQLDECIMAL	((SQLCOLTYPE)3)
+#   define SQLINTEGER	((SQLCOLTYPE)4)
+#   define SQLSMINT	((SQLCOLTYPE)5)
+#   define SQLFLOAT	((SQLCOLTYPE)6)
+#   define SQLREAL	((SQLCOLTYPE)7)
+#   define SQLDBLPREC	((SQLCOLTYPE)8)
+#   define SQLDATE	((SQLCOLTYPE)(-(U_DATE)))
+#   define SQLAMOUNT	((SQLCOLTYPE)(-(U_AMT)))
+#   define SQLHUGEAMT	((SQLCOLTYPE)(-(U_HAMT)))
+#   define SQLSMTIME	((SQLCOLTYPE)(-(U_TIME)))
+#   define SQLTEXT	((SQLCOLTYPE)(-(U_VTXT)))
+#   define SQLBINARY	((SQLCOLTYPE)(-(U_VBIN)))
+#   define SQLHDATE	((SQLCOLTYPE)(-(U_HDATE)))
+#   define SQLBYTE	((SQLCOLTYPE)(-(U_BYT)))
+#   define SQLINT64	((SQLCOLTYPE)(-(U_GINT)))
+#   define SQLAMT64	((SQLCOLTYPE)(-(U_GAMT)))
+
