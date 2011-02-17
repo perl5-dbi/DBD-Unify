b5ad67754db9809f8b11c731e212ce43285afe92
H.M. Brand <merijn@x1.(none)>
2011-02-17 16:00:21 +0100

    use correct quotes

--- lib/DBD/Unify.pm
+++ lib/DBD/Unify.pm
@@ -174,11 +174,11 @@ sub parse_trace_flag
 {
     my ($dbh, $name) = @_;
   # print STDERR "# Flags: $name\n";
-    return 0x7FFFFF00 if $name eq 'DBD';	# $h->trace ("DBD"); -- ALL
-  # return 0x01000000 if $name eq 'select';	# $h->trace ("SQL|select");
-  # return 0x02000000 if $name eq 'update';	# $h->trace ("1|update");
-  # return 0x04000000 if $name eq 'delete';
-  # return 0x08000000 if $name eq 'insert';
+    return 0x7FFFFF00 if $name eq "DBD";	# $h->trace ("DBD"); -- ALL
+  # return 0x01000000 if $name eq "select";	# $h->trace ("SQL|select");
+  # return 0x02000000 if $name eq "update";	# $h->trace ("1|update");
+  # return 0x04000000 if $name eq "delete";
+  # return 0x08000000 if $name eq "insert";
     return $dbh->SUPER::parse_trace_flag ($name);
     } # parse_trace_flag
 
