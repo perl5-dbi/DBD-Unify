5bbcc59eb0de0a1452d4e85e884958287caf813e
H.Merijn Brand <h.m.brand@procura.nl>
2014-01-01 12:52:05 +0100

    Up copyright to 2014

--- lib/DBD/Unify.pm
+++ lib/DBD/Unify.pm
@@ -1,4 +1,4 @@
-#   Copyright (c) 1999-2013 H.Merijn Brand
+#   Copyright (c) 1999-2014 H.Merijn Brand
 #
 #   You may distribute under the terms of either the GNU General Public
 #   License or the Artistic License, as specified in the Perl README file.
@@ -990,7 +990,7 @@ Todd Zervas has given a lot of feedback and patches.
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 1999-2013 H.Merijn Brand
+Copyright (C) 1999-2014 H.Merijn Brand
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself.
