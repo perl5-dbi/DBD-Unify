785d7f9df821f4f2bfd9e2ae327bf404d1144e83
H.Merijn Brand <h.m.brand@procura.nl>
2015-02-11 16:59:33 +0100

    Move project to github

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -78,7 +78,7 @@ test_recommends:
     Test::More:          1.001014
 resources:
     license:             http://dev.perl.org/licenses/
-    repository:          http://repo.or.cz/w/DBD-Unify.git
+    repository:          https://github.com/perl5-dbi/DBD-Unify
 meta-spec:
     version:             1.4
     url:                 http://module-build.sourceforge.net/META-spec-v1.4.html
