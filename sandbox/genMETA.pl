ecc6d3d9e2013252e7c7d9f0736cba779d1e28ec
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2017-06-17 10:43:41 +0200

    versions

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -73,9 +73,9 @@ test_requires:
     Test::More:          0.90
 recommends:
     perl:                5.020001
-    DBI:                 1.633
+    DBI:                 1.636
 test_recommends:
-    Test::More:          1.001014
+    Test::More:          1.302085
 resources:
     license:             http://dev.perl.org/licenses/
     repository:          https://github.com/perl5-dbi/DBD-Unify
