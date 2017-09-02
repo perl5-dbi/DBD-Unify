ff5c792dba8589fcd94e93b2559ba0ac47cc7069
H.Merijn Brand - Tux <h.m.brand@xs4all.nl>
2017-09-02 15:11:50 +0200

    ignore database dumps

---
+++ sandbox/dump_sys.pl
@@ -0,0 +1,15 @@
+#!/pro/bin/perl
+
+use 5.18.2;
+use warnings;
+
+use CSV;
+use PROCURA::DBD;
+
+my $dbh = DBDlogon;
+
+foreach my $t (sort grep m/\bSYS\b/ => $dbh->tables (undef, undef, undef, undef)) {
+    $t =~ s/"//g;
+    my $sth = prepex ("select * from $t");
+    csv (in => sub { $sth->fetchrow_hashref }, out => "dta/$t");
+    }
