786efe85ffebc989b8bf5f7d7aa17e517a7103b7
H.Merijn Brand - Tux <linux@tux.freedom.nl>
2024-01-05 16:59:55 +0100

    Meta merge

--- sandbox/genMETA.pl
+++ sandbox/genMETA.pl
@@ -42,7 +42,7 @@ version:                 VERSION
 abstract:                DBI driver for Unify database systems
 license:                 perl
 author:              
-    - H.Merijn Brand <h.m.brand@xs4all.nl>
+    - H.Merijn Brand <hmbrand@cpan.org>
 generated_by:            Author
 distribution_type:       module
 provides:
